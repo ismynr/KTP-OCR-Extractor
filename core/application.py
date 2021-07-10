bfcd1c4918ceeb09bcb5082228faf8eba4ec5acb
ismynr <gekkai7is.my@gmail.com>
2021-07-10 10:36:54 +0700

    update validation + change lang

--- core/application.py
+++ core/application.py
@@ -12,11 +12,13 @@ import textdistance #belum ada di requirenments
 import pandas as pd
 from operator import itemgetter, attrgetter
 import json
+import datetime
 
-# 路径配置
+# Konfigurasi jalur
 ROOT_PATH = os.getcwd()
 LINE_REC_PATH = os.path.join(ROOT_PATH, 'data/ID_CARD_KEYWORD.csv')
 CITIES_REC_PATH = os.path.join(ROOT_PATH, 'data/CITIES.csv')
+DISTRICTS_REC_PATH = os.path.join(ROOT_PATH, 'data/DISTRICTS.csv')
 RELIGION_REC_PATH = os.path.join(ROOT_PATH, 'data/RELIGION.csv')
 MARRIAGE_REC_PATH = os.path.join(ROOT_PATH, 'data/MARRIAGE.csv')
 NEED_COLON = [3, 4, 6, 8, 10, 11, 12, 13, 14, 15, 17, 18, 19, 21]
@@ -46,7 +48,7 @@ def allowed_image(filename):
 	else:
 		return False
 
-# 对身份证上的信息进行初步OCR
+# Lakukan OCR awal pada informasi di kartu ID
 def ocr_raw(image_path):
 	# (1) Read
 	img_raw = cv2.imdecode(np.fromstring(image_path.read(), np.uint8), cv2.IMREAD_UNCHANGED)
@@ -62,7 +64,7 @@ def ocr_raw(image_path):
 	result_raw = pytesseract.image_to_string(threshed, lang="ind")
 	return result_raw, id_number
 
-# 去除result_raw中的无效信息
+# Hapus informasi yang tidak valid di result_raw
 def strip_op(result_raw):
 	result_list = result_raw.split('\n')
 	new_result_list = []
@@ -73,8 +75,8 @@ def strip_op(result_raw):
 
 
 # KTP_ number
-# NIK_NUMBER模版匹配识别
-# 从左到右进行排列识别
+# NIK_NUMBER pengenalan pencocokan template
+# Atur pengakuan dari kiri ke kanan
 def sort_contours(cnts, method="left-to-right"):
 	reverse = False
 	i = 0
@@ -82,52 +84,52 @@ def sort_contours(cnts, method="left-to-right"):
 		reverse = True
 	if method == "top-to-bottom" or method == "bottom-to-top":
 		i = 1
-	boundingBoxes = [cv2.boundingRect(c) for c in cnts]  # 用一个最小的矩形，把找到的形状包起来x,y,h,w
+	boundingBoxes = [cv2.boundingRect(c) for c in cnts]  # Bungkus bentuk yang ditemukan dengan persegi panjang terkecil x,y,h,w
 	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
 										key=lambda b: b[1][i], reverse=reverse))
 	return cnts, boundingBoxes
 
 
 def return_id_number(image, gray):
-	# ret, thresh1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)#二值处理
-	# 定义核函数
-	rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))  # 返回矩形核函数
+	# ret, thresh1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)#Pemrosesan dua nilai
+	# Tentukan fungsi kernel
+	rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))  # Kembalikan fungsi kernel persegi panjang
 	# sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
-	tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)  # 顶帽
+	tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)  # Pucuk topi
 	gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
-	# ksize=-1相当于用3*3的 sobel过滤器，由于 Sobel 函数求导数后会有负值，还会有大于 255 的值，而原图像是 uint8 ，
-	# 所以 Sobel 建立的图像位数不够，会有截断。因此要使用 16 位有符号的数据类型，即 cv2.CV_16S
+	# ksize=-1 setara dengan menggunakan filter sobel 3*3, karena fungsi Sobel akan memiliki nilai negatif setelah turunan, dan akan ada nilai yang lebih besar dari 255, dan gambar aslinya adalah uint8,
+	# Jadi gambar yang dibuat oleh Sobel tidak memiliki cukup bit dan akan terpotong. Jadi gunakan tipe data bertanda 16-bit, yaitu cv2.CV_16S
 	gradX = np.absolute(gradX)
 	(minVal, maxVal) = (np.min(gradX), np.max(gradX))
 	gradX = (255 * ((gradX - minVal) / (maxVal - minVal)))
-	gradX = gradX.astype("uint8")  # Sobel之后无法显示，将其转回原来的 uint8 形式，否则无法显示图像
+	gradX = gradX.astype("uint8")  # Sobel tidak dapat ditampilkan setelahnya, kembalikan ke format uint8 asli, jika tidak, gambar tidak dapat ditampilkan
 	# print (np.array(gradX).shape)
-	# 通过闭操作（先膨胀，再腐蚀）将数字连在一起
+	# Hubungkan nomor bersama-sama dengan menutup operasi (perluas dulu, lalu korosi)
 	gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
-	# THRESH_OTSU会自动寻找合适的阈值，适合双峰，需把阈值参数设置为0
+	# THRESH_OTSU akan secara otomatis menemukan ambang batas yang sesuai, yang cocok untuk puncak ganda, dan parameter ambang batas perlu diatur ke 0
 	thresh = cv2.threshold(gradX, 0, 255,
 						   cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-	# 再来一个闭操作
-	thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, rectKernel)  # 再来一个闭操作
-	threshCnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 寻找轮廓
+	# Satu lagi operasi penutupan
+	thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, rectKernel) # Satu lagi operasi penutupan
+	threshCnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Temukan kontur
 	cnts = threshCnts
 	cur_img = image.copy()
 	cv2.drawContours(cur_img, cnts, -1, (0, 0, 255), 3)
 	copy = image.copy()
 	locs = []
-	# 遍历轮廓
+	# Lintasi kontur
 	for (i, c) in enumerate(cnts):
-		# 计算矩形
+		# Hitung persegi panjang
 		(x, y, w, h) = cv2.boundingRect(c)
 		ar = w / float(h)
-		# 选择合适的区域，根据实际任务来，这里的基本都是四个数字一组
+		# Pilih area yang tepat, sesuai dengan tugas yang sebenarnya, di sini pada dasarnya adalah sekelompok empat angka
 		# if ar >10:
 		# if (w > 40 ) and (h > 10 and h < 20):
-		# 符合的留下来
+		# Tetap dalam antrean
 		if h > 10 and w > 100 and x < 300:
 			img = cv2.rectangle(copy, (x, y), (x + w, y + h), (0, 255, 0), 2)
 			locs.append((x, y, w, h, w * h))
-	# 将符合的轮廓按照面积从大到小排序
+	# Urutkan kontur yang cocok berdasarkan area dari terbesar ke terkecil
 	locs = sorted(locs, key=itemgetter(1), reverse=False)
 
 	# print(locs[1][0])
@@ -137,32 +139,32 @@ def return_id_number(image, gray):
 	# print(nik)
 	text = image[locs[2][1] - 10:locs[2][1] + locs[2][3] + 10, locs[2][0] - 10:locs[2][0] + locs[2][2] + 10]
 
-	# 读取一个模板图像
+	# Baca gambar template
 	img = cv2.imread("images/module.jpeg")
-	# 灰度图
+	# Skala abu-abu
 	ref = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	# 二值图像
+	# Gambar biner
 	ref = cv2.threshold(ref, 66, 255, cv2.THRESH_BINARY_INV)[1]
-	# 计算轮廓
-	# cv2.findContours()函数接受的参数为二值图，即黑白的（不是灰度图）,cv2.RETR_EXTERNAL只检测外轮廓，cv2.CHAIN_APPROX_SIMPLE只保留终点坐标
-	# 返回的list中每个元素都是图像中的一个轮廓
+	# Hitung kontur
+	# cv2.findContours()Parameter yang diterima oleh fungsi adalah citra biner, yaitu hitam putih (bukan skala abu-abu), cv2.RETR_EXTERNAL hanya mendeteksi kontur luar, cv2.CHAIN_APPROX_SIMPLE hanya mempertahankan koordinat titik akhir
+	# Setiap elemen dalam daftar yang dikembalikan adalah garis besar dalam gambar
 	refCnts, hierarchy = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	# cv2.drawContours(img, refCnts, -1, (0, 0, 255), 3)#上颜色
 	# cv2.imshow('888', img)
 	# cv2.waitKey(0)
 	# cv2.destroyAllWindows()
 
-	refCnts = sort_contours(refCnts, method="left-to-right")[0]  # 排序，从左到右，从上到下
+	refCnts = sort_contours(refCnts, method="left-to-right")[0]  # Urutkan, dari kiri ke kanan, atas ke bawah
 	digits = {}
 
-	# 遍历每一个轮廓
+	# Lintasi setiap kontur
 	for (i, c) in enumerate(refCnts):
-		# 计算外接矩形并且resize成合适大小
+		# Hitung persegi panjang pembatas dan ubah ukurannya ke ukuran yang sesuai
 		(x, y, w, h) = cv2.boundingRect(c)
 		# if
 		roi = ref[y:y + h, x:x + w]
 		roi = cv2.resize(roi, (57, 88))
-		# 每一个数字对应每一个模板
+		# Setiap nomor sesuai dengan setiap template
 		digits[i] = roi
 	# cv_show('digits[i]', digits[i])
 	# nik = np.clip(nik, 0, 255)
@@ -176,14 +178,14 @@ def return_id_number(image, gray):
 	group = cv2.threshold(gray_nik, 127, 255, cv2.THRESH_BINARY_INV)[1]
 	# cv2.imshow('9', group)
 
-	# 计算每一组的轮廓
+	# Hitung kontur setiap grup
 	digitCnts, hierarchy_nik = cv2.findContours(group.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 	nik_r = nik.copy()
 
 	cv2.drawContours(nik_r, digitCnts, -1, (0, 0, 255), 3)
 
-	# 定位NIK的位置
+	# Cari lokasi NIK
 	gX = locs[1][0]
 	gY = locs[1][1]
 	gW = locs[1][2]
@@ -192,12 +194,12 @@ def return_id_number(image, gray):
 	ctx = sort_contours(digitCnts, method="left-to-right")[0]
 
 	locs_x = []
-	# 遍历轮廓
+	# Lintasi kontur
 	for (i, c) in enumerate(ctx):
-		# 计算矩形
+		# Hitung persegi panjang
 		(x, y, w, h) = cv2.boundingRect(c)
 
-		# 选择合适的区域，根据实际任务来，这里的基本都是四个数字一组
+		# Pilih area yang tepat, sesuai dengan tugas yang sebenarnya, di sini pada dasarnya adalah sekelompok empat angka
 
 		if h > 10 and w > 10:
 			img = cv2.rectangle(nik_r, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -206,37 +208,37 @@ def return_id_number(image, gray):
 	# digitCnts = sort_contours(digitCnts, method="left-to-right")[0]
 	output = []
 	groupOutput = []
-	# 计算每一组中的每一个数值
+	# Hitung setiap nilai di setiap grup
 	for c in locs_x:
-		# 找到当前数值的轮廓，resize成合适的的大小
+		# Temukan kontur nilai saat ini dan ubah ukuran ke ukuran yang sesuai
 		(x, y, w, h) = c
 		roi = group[y:y + h, x:x + w]
 		roi = cv2.resize(roi, (57, 88))
 		# cv_show('roi',roi)
 
-		# 计算匹配得分
+		# Hitung skor pertandingan
 		scores = []
 
-		# 在模板中计算每一个得分
+		# Hitung setiap skor dalam template
 		for (digit, digitROI) in digits.items():
-			# 模板匹配
+			# Pencocokan template
 			result = cv2.matchTemplate(roi, digitROI,
 									   cv2.TM_CCOEFF)
 			(_, score, _, _) = cv2.minMaxLoc(result)
 			scores.append(score)
 
-		# 得到最合适的数字
+		# Dapatkan nomor yang paling cocok
 
 		groupOutput.append(str(np.argmax(scores)))
 
-	# 画出来
+	# Seri
 	cv2.rectangle(image, (gX - 5, gY - 5),
 				  (gX + gW + 5, gY + gH + 5), (0, 0, 255), 1)
 	cv2.putText(image, "".join(groupOutput), (gX, gY - 15),
 				cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-	# 图像，文字内容， 坐标 ，字体，大小，颜色，字体厚度
+	# Gambar, konten teks, koordinat, font, ukuran, warna, ketebalan font
 
-	# 得到结果
+	#mendapat jawabannya
 	output.extend(groupOutput)
 	return ''.join(output)
 	
@@ -271,24 +273,16 @@ def extract_ktp():
 			}
 
 		if allowed_image(image.filename):
-			# img = cv2.imdecode(np.fromstring(request.files['ktp'].read(), np.uint8), cv2.IMREAD_UNCHANGED)  
-			# return {
-			# 	'msg': image.filename
-			# }
-			# img = cv2.imdecode(np.fromstring(request.files['ktp'].read(), np.uint8), cv2.IMREAD_UNCHANGED)  
-			# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-			# th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)
-			# result = pytesseract.image_to_string((threshed), lang="ind")
-			# result.replace('\n', ' ')
-
 			IMAGE_PATH = image
 
 			raw_df = pd.read_csv(LINE_REC_PATH, header=None)
-			# 读入城市字典
+			# Baca kamus kota
 			cities_df = pd.read_csv(CITIES_REC_PATH, header=None)
-			# 读入宗教字典
+			# Baca kamus desa
+			districts_df = pd.read_csv(DISTRICTS_REC_PATH, header=None)
+			# Baca kamus agama
 			religion_df = pd.read_csv(RELIGION_REC_PATH, header=None)
-			# 读入婚姻字典
+			# Baca kamus pernikahan
 			marriage_df = pd.read_csv(MARRIAGE_REC_PATH, header=None)
 			result_raw, id_number = ocr_raw(IMAGE_PATH)
 			result_list = strip_op(result_raw)
@@ -303,10 +297,10 @@ def extract_ktp():
 					tmp_sim_np = np.asarray(tmp_sim_list)
 					arg_max = np.argmax(tmp_sim_np)
 					if tmp_sim_np[arg_max] >= 0.6:
-						loc2index[(i, j)] = arg_max  ##最高相似度
+						loc2index[(i, j)] = arg_max  # Kesamaan tertinggi
 
 			# PROSES EXTRAKSI KTP 
-			# 数据处理
+			# pengolahan data
 			last_result_list = []
 			useful_info = False
 			for i, tmp_line in enumerate(result_list):
@@ -315,7 +309,7 @@ def extract_ktp():
 					tmp_word = tmp_word.strip(':')
 					if (i, j) in loc2index:
 						useful_info = True
-						if loc2index[(i, j)] == NEXT_LINE:  ##换行
+						if loc2index[(i, j)] == NEXT_LINE:  # Garis baru
 							last_result_list.append(tmp_list)
 							tmp_list = []
 						tmp_list.append(raw_df[0].values[loc2index[(i, j)]])
@@ -335,19 +329,30 @@ def extract_ktp():
 			for tmp_data in last_result_list:
 				if '—' in tmp_data:
 					tmp_data.remove('—')
-				# 对前两行进行处理
+				# Proses dua baris pertama
 				if 'PROVINSI' in tmp_data or 'KABUPATEN' in tmp_data or 'KOTA' in tmp_data:
 					for tmp_index, tmp_word in enumerate(tmp_data[1:]):
-						# 计算相似度
+						# Hitung kesamaan
 						tmp_sim_list = [textdistance.damerau_levenshtein.normalized_similarity(tmp_word, tmp_word_) for
 										tmp_word_ in cities_df[0].values]
 						tmp_sim_np = np.asarray(tmp_sim_list)
-						# 获得相似度最大单词索引
+						# Dapatkan indeks kesamaan kata maksimum
 						arg_max = np.argmax(tmp_sim_np)
 						if tmp_sim_np[arg_max] >= 0.6:
 							tmp_data[tmp_index + 1] = cities_df[0].values[arg_max]
 
-				# 对身份证一行进行处理
+				if 'KECAMATAN' in tmp_data:
+					for tmp_index, tmp_word in enumerate(tmp_data[1:]):
+						# Hitung kesamaan
+						tmp_sim_list = [textdistance.damerau_levenshtein.normalized_similarity(tmp_word, tmp_word_) for
+										tmp_word_ in districts_df[0].values]
+						tmp_sim_np = np.asarray(tmp_sim_list)
+						# Dapatkan indeks kesamaan kata maksimum
+						arg_max = np.argmax(tmp_sim_np)
+						if tmp_sim_np[arg_max] >= 0.6:
+							tmp_data[tmp_index + 1] = districts_df[0].values[arg_max]
+
+				# Proses jalur KTP
 				if 'NIK' in tmp_data:
 					if len(id_number) != 16:
 						id_number = tmp_data[2]
@@ -365,26 +370,26 @@ def extract_ktp():
 							tmp_data.pop()
 						tmp_data[2] = id_number
 
-				# 对宗教一行进行处理
+				# Proses garis agama
 				if 'Agama' in tmp_data:
 					for tmp_index, tmp_word in enumerate(tmp_data[1:]):
-						# 计算相似度
+						# Hitung kesamaan
 						tmp_sim_list = [textdistance.damerau_levenshtein.normalized_similarity(tmp_word, tmp_word_) for
 										tmp_word_ in religion_df[0].values]
 						tmp_sim_np = np.asarray(tmp_sim_list)
-						# 获得相似度最大单词索引
+						# Dapatkan indeks kata yang paling mirip
 						arg_max = np.argmax(tmp_sim_np)
 						if tmp_sim_np[arg_max] >= 0.6:
 							tmp_data[tmp_index + 1] = religion_df[0].values[arg_max]
 
-				# 对婚姻一行进行处理
+				# Berurusan dengan garis pernikahan
 				if 'Status' in tmp_data or 'Perkawinan' in tmp_data:
 					for tmp_index, tmp_word in enumerate(tmp_data[2:]):
-						# 计算相似度
+						# Hitung kesamaan
 						tmp_sim_list = [textdistance.damerau_levenshtein.normalized_similarity(tmp_word, tmp_word_) for
 										tmp_word_ in marriage_df[0].values]
 						tmp_sim_np = np.asarray(tmp_sim_list)
-						# 获得相似度最大单词索引
+						# Dapatkan indeks kata yang paling mirip
 						arg_max = np.argmax(tmp_sim_np)
 						if tmp_sim_np[arg_max] >= 0.6:
 							tmp_data[tmp_index + 2] = marriage_df[0].values[arg_max]
@@ -411,7 +416,6 @@ def extract_ktp():
 					'origin' : last_result_list,
 					'message':'Atributte KTP ada yang tidak terdeteksi, mohon upload ulang !'
 				}
-
 			# ngecek atribut yang dipakai --------- END
 
 			# mengambil atribut --------- START
@@ -421,10 +425,25 @@ def extract_ktp():
 					string = ' '.join(tmp_data)
 					if 'PROVINSI' in string:
 						fPROVINSI = string.replace('PROVINSI ', '')
+						fPROVINSI = alpha_with_remove_first_end_spaces(fPROVINSI)
+						if fPROVINSI is None:
+							raise ValueError('Provinsi pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
+						if special_match(fPROVINSI) is False:
+							raise ValueError('Provinsi pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
 					if 'KABUPATEN' in string:
 						fKABUPATEN_KOTA = string.replace('KABUPATEN ', '')
+						fKABUPATEN_KOTA = alpha_with_remove_first_end_spaces(fKABUPATEN_KOTA)
+						if fKABUPATEN_KOTA is None:
+							raise ValueError('Kabupaten pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
+						if special_match(fKABUPATEN_KOTA) is False:
+							raise ValueError('Kabupaten pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
 					if 'KOTA' in string:
 						fKABUPATEN_KOTA = string.replace('KOTA ', '')
+						fKABUPATEN_KOTA = alpha_with_remove_first_end_spaces(fKABUPATEN_KOTA)
+						if fKABUPATEN_KOTA is None:
+							raise ValueError('Kota pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
+						if special_match(fKABUPATEN_KOTA) is False:
+							raise ValueError('Kota pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
 					# PENTING DIPAKE
 					if 'NIK' in string:
 						fNIK = string.replace('NIK : ', '')
@@ -445,8 +464,9 @@ def extract_ktp():
 						word = string.replace('Tempat/Tgl Lahir : ', '')
 						if re.search("([0-9]{2}\-[0-9]{2}\-[0-9]{4})", word) is None:
 							raise ValueError('Tempat Lahir pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
-						fTANGGAL_LAHIR = re.search("([0-9]{2}\-[0-9]{2}\-[0-9]{4})", word)[0]
-						word = word.replace(fTANGGAL_LAHIR, '')
+						tglLahirReplace = re.search("([0-9]{2}\-[0-9]{2}\-[0-9]{4})", word)[0]
+						fTANGGAL_LAHIR = datetime.datetime.strptime(tglLahirReplace, '%d-%m-%Y').strftime('%Y-%m-%d')
+						word = word.replace(tglLahirReplace, '')
 						fTEMPAT = alpha_with_remove_first_end_spaces(word)
 						if fTEMPAT is None:
 							raise ValueError('Tempat Lahir pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
@@ -461,10 +481,16 @@ def extract_ktp():
 					# PENTING DIPAKE
 					if 'RT/RW' in string:
 						word = string.replace("RT/RW : ",'')
-						if re.search('/', word) is None:
+						if re.search(' +', word) is None:
 							raise ValueError('RT dan RW pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
-						fRT = word.split('/')[0].strip()
-						fRW = word.split('/')[1].strip()
+						else:
+							fRT = word.split(' ')[0].strip()
+							fRW = word.split(' ')[1].strip()
+							if re.search('/', word):
+								if re.search('/', word) is None:
+									raise ValueError('RT dan RW pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
+								fRT = word.split('/')[0].strip()
+								fRW = word.split('/')[1].strip()
 						if fRT is None:
 							raise ValueError('RT pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
 						if numeric_match(fRT) is False:
@@ -483,6 +509,11 @@ def extract_ktp():
 							raise ValueError('Kel Desa pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
 					if 'Kecamatan' in string:
 						fKECAMATAN = string.replace('Kecamatan : ', '')
+						fKECAMATAN = alpha_with_remove_first_end_spaces(fKECAMATAN)
+						if fKECAMATAN is None:
+							raise ValueError('Kecamatan pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
+						if special_match(fKECAMATAN) is False:
+							raise ValueError('Kecamatan pada KTP kurang jelas, silahkan foto dan upload ulang KTP')
 					# PENTING DIPAKE
 					if 'Agama' in string:
 						fAGAMA = string.replace('Agama : ', '')
@@ -508,6 +539,7 @@ def extract_ktp():
 				except ValueError as e:
 					return {
 						'success':False,
+						'origin' : last_result_list,
 						'message': str(e)
 					}
 				except Exception:
